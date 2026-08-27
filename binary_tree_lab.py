c06664dd96c7829f0944f5fcdc87114dd43473c1
Jordan Mays <jmays12@gmail.com>
2026-08-27 14:53:40 -0500

    Add LCA and max_depth search functionality

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -1,3 +1,4 @@
+from re import L
 from typing import Optional
 
 class TreeNode:
@@ -8,8 +9,22 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if not root:
+        return 0
+    
+    l = max_depth(root.left)
+    r = max_depth(root.right)
+
+    return (max(l, r)) + 1
+
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    if not root:
+        return None
+    if root.val > p.val and root.val > q.val:
+        return lowest_common_ancestor(root.left, p, q)
+    elif root.val < p.val and root.val < q.val:  
+        return lowest_common_ancestor(root.right, p, q)
+    else:
+        return root
